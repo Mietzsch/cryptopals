e5c2d14a6b4466242700c2099f5ce89683d96eec
Mietzsch <nmietzsch@gmail.com>
2021-11-20 15:39:14 +0100

    s02e02

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use cryptopals::s02::padding::pkcs7_padding;
+use std::fs;
+
+use cryptopals::{s02::aes_cbc::aes128_cbc_decode, util::base_64::Base64};
 
 fn main() {
-    let input = String::from("YELLOW SUBMARINE");
+    let key = String::from("YELLOW SUBMARINE");
 
-    let res = pkcs7_padding(input.as_bytes(), 20);
+    let iv = [0; 16];
 
-    println!("{}", hex::encode(res));
+    let input = fs::read_to_string("data/set2/2.txt")
+        .expect("Something went wrong reading the challenge file");
+
+    let input = input.replace("\n", "");
+
+    let input_bytes = Base64::new_from_string(&input).unwrap();
+
+    let decoded = aes128_cbc_decode(input_bytes.to_bytes(), key.as_bytes(), &iv);
+
+    println!("Plaintext:\n{}", std::str::from_utf8(&decoded).unwrap())
 }
---
+++ src/s02/aes_cbc.rs
@@ -0,0 +1,79 @@
+use aes::{
+    cipher::generic_array::GenericArray, Aes128, Block, BlockDecrypt, BlockEncrypt, NewBlockCipher,
+};
+
+use crate::util::xor;
+
+pub fn aes128_cbc_encode(plain: &[u8], key: &[u8], iv: &[u8]) -> Vec<u8> {
+    let key_array = GenericArray::from_slice(key);
+    let aes = Aes128::new(key_array);
+
+    let mut res = Vec::<u8>::new();
+
+    let mut xor_text = Vec::<u8>::from(iv);
+
+    if plain.len() % 16 != 0 {
+        panic!("input must be multiple of 16!");
+    }
+
+    for chunk in plain.chunks(16) {
+        let mut block = Block::clone_from_slice(&xor::xor(chunk, &xor_text));
+        aes.encrypt_block(&mut block);
+        xor_text = Vec::<u8>::from(block.to_vec());
+        res.append(&mut block.to_vec());
+    }
+
+    res
+}
+
+pub fn aes128_cbc_decode(cipher: &[u8], key: &[u8], iv: &[u8]) -> Vec<u8> {
+    let key_array = GenericArray::from_slice(key);
+    let aes = Aes128::new(key_array);
+
+    let mut res = Vec::<u8>::new();
+
+    let mut xor_text = Vec::<u8>::from(iv);
+
+    if cipher.len() % 16 != 0 {
+        panic!("input must be multiple of 16!");
+    }
+
+    for chunk in cipher.chunks(16) {
+        let mut block = Block::clone_from_slice(chunk);
+        aes.decrypt_block(&mut block);
+        res.append(&mut xor::xor(&block.to_vec(), &xor_text));
+        xor_text = Vec::<u8>::from(chunk);
+    }
+
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use crate::util::base_64::Base64;
+
+    use super::*;
+
+    #[test]
+    fn s02e02() {
+        let key = String::from("YELLOW SUBMARINE");
+
+        let iv = [0; 16];
+
+        let input = fs::read_to_string("data/set2/2.txt")
+            .expect("Something went wrong reading the challenge file");
+
+        let input = input.replace("\n", "");
+
+        let input_bytes = Base64::new_from_string(&input).unwrap();
+
+        let decoded = aes128_cbc_decode(input_bytes.to_bytes(), key.as_bytes(), &iv);
+
+        let plain = fs::read_to_string("data/set1/7_plain.txt")
+            .expect("Something went wrong reading the challenge file");
+
+        assert_eq!(plain, std::str::from_utf8(&decoded).unwrap())
+    }
+}
