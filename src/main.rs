5c0566ffcd4528bf70fb49a7a94672c6885faec0
Mietzsch <nmietzsch@gmail.com>
2021-12-05 16:36:05 +0100

    s02e08

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,26 @@
-use std::{fs, str::from_utf8};
-
-use cryptopals::{
-    s02::ecb_decryption::{decrypt_ecb, ECBOracleHard},
-    util::{base_64::Base64, generators::generate_aes_key},
-};
-use rand::{Rng, RngCore};
+use cryptopals::{s02::admin::AdminOracleCBC, util::generators::generate_aes_key};
 
 fn main() {
-    let input = fs::read_to_string("data/set2/4.txt")
-        .expect("Something went wrong reading the challenge file");
-    let input = input.replace("\n", "");
+    let prefix = b"comment1=cooking MCs;userdata=";
+    let postfix = b";comment2= like a pound of bacon";
 
-    let random_vec_len = rand::thread_rng().gen_range(0..32);
-    let mut random_vec = vec![0; random_vec_len];
-    rand::thread_rng().fill_bytes(&mut random_vec);
+    let admin_oracle = AdminOracleCBC::new(&generate_aes_key(), prefix, postfix);
 
-    let oracle = ECBOracleHard::new(
-        Base64::new_from_string(&input).unwrap().to_bytes(),
-        &generate_aes_key(),
-        &random_vec,
-    );
+    let pad = [0; 2];
+    let zero_block = [0; 16];
+    let admin = b"\0\0\0\0\0\0admin\0true";
 
-    let dec = decrypt_ecb(&oracle);
+    let mut input = Vec::new();
+    input.append(&mut pad.to_vec());
+    input.append(&mut zero_block.to_vec());
+    input.append(&mut admin.to_vec());
 
-    let dec_str = from_utf8(&dec).unwrap();
+    let mut encr = admin_oracle.encrypt(&input);
 
-    println!("Decoded:\n{}", dec_str);
+    encr.1[prefix.len() + pad.len() + 5] ^= b';';
+    encr.1[prefix.len() + pad.len() + 11] ^= b'=';
+
+    let success = admin_oracle.is_admin(&encr.0, &encr.1);
+
+    println!("Success: {}", success);
 }
--- src/s02/admin.rs
+++ src/s02/admin.rs
@@ -6,6 +6,8 @@ use crate::{
     util::generators::generate_aes_key,
 };
 
+use super::aes_cbc::{aes128_cbc_decode, aes128_cbc_encode};
+
 pub struct AdminOracle {
     key: Vec<u8>,
 }
@@ -26,6 +28,42 @@ impl AdminOracle {
             plain.truncate(plain.len() - last_byte as usize);
         }
         from_utf8(&plain).unwrap().to_string()
+    }
+}
+
+pub struct AdminOracleCBC {
+    key: Vec<u8>,
+    prefix: Vec<u8>,
+    postfix: Vec<u8>,
+}
+
+impl AdminOracleCBC {
+    pub fn new(key: &[u8], prefix: &[u8], postfix: &[u8]) -> AdminOracleCBC {
+        AdminOracleCBC {
+            key: key.to_vec(),
+            prefix: prefix.to_vec(),
+            postfix: postfix.to_vec(),
+        }
+    }
+
+    pub fn encrypt(&self, input: &[u8]) -> (Vec<u8>, Vec<u8>) {
+        let mut plain = Vec::new();
+        plain.append(&mut self.prefix.clone());
+        plain.append(&mut input.to_vec());
+        plain.append(&mut self.postfix.clone());
+
+        let iv = generate_aes_key();
+        (iv.clone(), aes128_cbc_encode(&plain, &self.key, &iv))
+    }
+
+    pub fn is_admin(&self, iv: &[u8], cipher: &[u8]) -> bool {
+        let plain = aes128_cbc_decode(cipher, &self.key, iv);
+        for substring in plain.split(|byte| *byte == b';') {
+            if substring == b"admin=true" {
+                return true;
+            }
+        }
+        false
     }
 }
 
@@ -62,5 +100,36 @@ mod tests {
     #[test]
     fn s02e05() {
         assert_eq!(cut_and_paste().contains("role=admin"), true);
+    }
+
+    #[test]
+    fn s02e08() {
+        let prefix = b"comment1=cooking MCs;userdata=";
+        let postfix = b";comment2= like a pound of bacon";
+
+        let admin_oracle = AdminOracleCBC::new(&generate_aes_key(), prefix, postfix);
+
+        let pad = [0; 2];
+        let zero_block = [0; 16];
+
+        let mut admin = b"\0\0\0\0\0;admin=true".to_vec();
+        let semi_position = 5;
+        let equal_position = 11;
+        admin[semi_position] ^= b';';
+        admin[equal_position] ^= b'=';
+
+        let mut input = Vec::new();
+        input.append(&mut pad.to_vec());
+        input.append(&mut zero_block.to_vec());
+        input.append(&mut admin);
+
+        let mut encr = admin_oracle.encrypt(&input);
+
+        encr.1[prefix.len() + pad.len() + semi_position] ^= b';';
+        encr.1[prefix.len() + pad.len() + equal_position] ^= b'=';
+
+        let success = admin_oracle.is_admin(&encr.0, &encr.1);
+
+        assert!(success);
     }
 }
